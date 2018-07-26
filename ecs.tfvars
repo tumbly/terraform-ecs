14ede7e58480f406a15bafd011eb6d538bd47cc6
Eric Boismard <eboismard@bcstechno.com>
2018-07-26 15:14:00 +0200

    up

--- ecs.tfvars
+++ ecs.tfvars
@@ -8,11 +8,11 @@ private_subnet_cidrs = ["10.0.50.0/24", "10.0.51.0/24", "10.0.52.0/24"]
 
 availability_zones = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
 
-max_size = 1
+max_size = 3
 
 min_size = 1
 
-desired_capacity = 1
+desired_capacity = 3
 
 ecs_key_name = "ebo"
 
--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -6,7 +6,7 @@ output "default_alb_target_group" {
   value = "${aws_alb_target_group.default.arn}"
 }
 
-output "alb_dns_name" {
-  description = "The DNS name of the load balancer."
-  value       = "${element(concat(aws_lb.application.*.dns_name, aws_lb.application_no_logs.*.dns_name), 0)}"
-}
+#output "alb_dns_name" {
+#  description = "The DNS name of the load balancer."
+#  value       = "${element(concat(aws_lb.application.*.dns_name, aws_lb.application_no_logs.*.dns_name), 0)}"
+#}
